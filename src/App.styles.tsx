f6129a7409721277987e9b942a7885c3aa830c96
Abiel Zulio M <abielzm@gmail.com>
2023-01-07 17:27:10 +0700

    feat: use local font

---
+++ src/App.styles.tsx
@@ -0,0 +1,86 @@
+import styled, { createGlobalStyle } from "styled-components"
+export const GlobalStyle = createGlobalStyle`
+    @font-face {
+    font-family: "TestGeograph";
+    src: local("TestGeograph"), url('/fonts/TestGeograph-Black.otf') format("opentype");
+    font-style: normal;
+    font-weight: 900;
+    font-display: swap;
+  }
+  @font-face {
+    font-family: "TestGeograph";
+    src: local("TestGeograph"), url('/fonts/TestGeograph-BlackItalic.otf') format("opentype");
+    font-style: italic;
+    font-weight: 900;
+    font-display: swap;
+  }
+  @font-face {
+    font-family: 'TestGeograph-Bold';
+    src: local("TestGeograph"), url('/fonts/TestGeograph-Bold.otf') format("opentype");
+    font-style: normal;
+    font-weight: 700;
+    font-display: swap;
+  }
+  @font-face {
+    font-family: 'TestGeograph-BoldItalic';
+    src: local("TestGeograph"), url('/fonts/TestGeograph-BoldItalic.otf') format("opentype");
+    font-style: italic;
+    font-weight: 700;
+    font-display: swap;
+  }
+  @font-face {
+    font-family: 'TestGeograph-Medium';
+    src: local("TestGeograph"), url('/fonts/TestGeograph-Medium.otf') format("opentype");
+    font-style: normal;
+    font-weight: 500;
+    font-display: swap;
+  }
+  @font-face {
+    font-family: 'TestGeograph-MediumItalic';
+    src: local("TestGeograph"), url('/fonts/TestGeograph-MediumItalic.otf') format("opentype");
+    font-style: italic;
+    font-weight: 500;
+    font-display: swap;
+  }
+  @font-face {
+    font-family: 'TestGeograph-Regular';
+    src: local("TestGeograph"), url('/fonts/TestGeograph-Regular.otf') format("opentype");
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+  }
+  @font-face {
+    font-family: 'TestGeograph-RegularItalic';
+    src: local("TestGeograph"), url('/fonts/TestGeograph-RegularItalic.otf') format("opentype");
+    font-style: italic;
+    font-weight: 400;
+    font-display: swap;
+  }
+  @font-face {
+    font-family: 'TestGeograph-Light';
+    src: local("TestGeograph"), url('/fonts/TestGeograph-Light.otf') format("opentype");
+    font-style: normal;
+    font-weight: 300;
+    font-display: swap;
+  }
+  @font-face {
+    font-family: 'TestGeograph-LightItalic';
+    src: local("TestGeograph"), url('/fonts/TestGeograph-LightItalic.otf') format("opentype");
+    font-style: italic;
+    font-weight: 300;
+    font-display: swap;
+  }
+  @font-face {
+    font-family: 'TestGeograph-Thin';
+    src: local("TestGeograph"), url('/fonts/TestGeograph-Thin.otf') format("opentype");
+    font-style: normal;
+    font-weight: 100;
+    font-display: swap;
+  }
+  @font-face {
+    font-family: 'TestGeograph-ThinItalic';
+    src: local("TestGeograph"), url('/fonts/TestGeograph-ThinItalic.otf') format("opentype");
+    font-style: italic;
+    font-weight: 100;
+    font-display: swap;
+  }
